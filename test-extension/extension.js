13247778730502a882c56fddfccab1277298c450
Lorenzo Murarotto <murar8@proton.me>
2025-10-19 11:59:07 +0200

    Simplify pack logic

--- test-extension/extension.js
+++ test-extension/extension.js
@@ -1,11 +1,10 @@
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 export default class TestExtension extends Extension {
-    enable() {
-        console.log('Test Extension enabled');
-    }
-
-    disable() {
-        console.log('Test Extension disabled');
-    }
+  enable() {
+    console.log("Test Extension enabled");
+  }
+  disable() {
+    console.log("Test Extension disabled");
+  }
 }
