2f182d3129cbf6ab9f69efb4f5324634e944de29
Lorenzo Murarotto <murar8@proton.me>
2025-10-25 11:51:25 +0200

    ci: test

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ set -u          # Exit on undefined variable
 set -o pipefail # Exit on pipeline error
 
 docker compose -f docker-compose.test.yml run --env INPUT_SOURCE_DIR=./test-extension --env INPUT_OUTPUT_DIR=./dist --rm --build --remove-orphans upload
+docker compose down --remove-orphans
 tmpdir=$(mktemp -d)
 test -f ./dist/test-extension@example.com.shell-extension.zip
 mkdir -p $tmpdir/extracted
