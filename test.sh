7fb8c89c2ecec9c06a90752c4d3121a1d3770671
Lorenzo Murarotto <murar8@proton.me>
2025-11-01 11:29:59 +0100

    ci: fix check

--- test.sh
+++ test.sh
@@ -6,11 +6,12 @@ set -o pipefail # Exit on pipeline error
 
 docker compose -f docker-compose.test.yml run --env INPUT_SOURCE_DIR=./test-extension --env INPUT_OUTPUT_DIR=./dist --rm --build --remove-orphans upload
 docker compose -f docker-compose.test.yml down --remove-orphans
-tmpdir=$(mktemp -d)
 test -f ./dist/test-extension@example.com.shell-extension.zip
+tmpdir=$(mktemp -d)
 mkdir -p $tmpdir/extracted
 unzip -q ./dist/test-extension@example.com.shell-extension.zip -d $tmpdir/extracted
 diff -r ./test-extension $tmpdir/extracted
-rm -rf $tmpdir
 
 echo "All tests passed!"
+
+rm -rf $tmpdir
